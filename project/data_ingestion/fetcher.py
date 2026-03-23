09f683aaa9a52b22b4a95bc4e24854f5ed76f411
Noulmon Kandanchira <noulmonkc@gmail.com>
2026-03-24 00:28:57 +0100

    feat(data-ingestion): add data ingestion pipeline

---
+++ project/data_ingestion/fetcher.py
@@ -0,0 +1,11 @@
+import requests
+
+def fetch_repo_zip(repo_owner, repo_name) -> bytes:
+    prefix = 'https://codeload.github.com'
+    url = f'{prefix}/{repo_owner}/{repo_name}/zip/refs/heads/main'
+    resp = requests.get(url)
+    
+    if resp.status_code != 200:
+        raise Exception(f"Failed to download repository: {resp.status_code}")
+    
+    return resp.content
