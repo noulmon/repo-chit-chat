b7e3bd807ec83a4e58d08e7986a2b52067df5a11
Noulmon Kandanchira <noulmonkc@gmail.com>
2026-03-26 21:12:24 +0100

    feat(search): Add text, vector & hybrid search

---
+++ project/search/retriever.py
@@ -0,0 +1,42 @@
+import numpy as np
+from minsearch import VectorSearch, Index
+
+
+
+class SearchEngine:
+    def __init__(self, embedding_model, chunks, embeddings):
+        self.embedding_model = embedding_model
+        self.chunks = chunks
+        self.embeddings = np.array(embeddings)
+
+        # build indexes once
+        self.index = Index(
+            text_fields=["chunk", "title", "description", "filename"],
+            keyword_fields=[]
+        )
+        self.index.fit(chunks)
+
+        self.vindex = VectorSearch()
+        self.vindex.fit(self.embeddings, chunks)
+
+    def text_search(self, query: str) -> list:
+        return self.index.search(query)
+
+    def vector_search(self, query: str) -> list:
+        q = self.embedding_model.encode(query)
+        return self.vindex.search(q, num_results=1)
+
+    def hybrid_search(self, query: str) -> list:
+        text_results = self.text_search(query)
+        vector_results = self.vector_search(query)
+        
+        seen = set()
+        combined_results = []
+        for result in text_results + vector_results:
+            # use a unique chunk identifier instead of filename
+            key = (result['filename'], result['chunk'])
+            if key not in seen:
+                seen.add(key)
+                combined_results.append(result)
+        
+        return combined_results
